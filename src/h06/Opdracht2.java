04b22dde3cc97ba50c1124af54b3ad043279028a
ochab <ochabab@roc-dev.com>
2019-09-30 09:27:05 +0200

    t/m h8

---
+++ src/h06/Opdracht2.java
@@ -0,0 +1,29 @@
+package h06;
+
+
+import java.awt.*;
+import java.applet.*;
+
+
+public class Opdracht2 extends Applet {
+    int a, b, c;
+    double Seconden;
+    double Minuut;
+    double Uur;
+
+
+    public void init() {
+        a = 60;
+        b = 60;
+        c = 24;
+        Seconden = a;
+        Minuut = b;
+        Uur = c;
+    }
+
+    public void paint(Graphics g) {
+        g.drawString("1 minuut is: " + Seconden , 20, 20);
+        g.drawString("1 uur is: " + Minuut , 40, 40);
+        g.drawString("1 dag is: " + Uur , 60, 60);
+    }
+}
