c1eaf6ddd2ff27ee25d00f0e4c96984b033f2b30
ochab <ochabab@roc-dev.com>
2019-10-08 11:00:25 +0200

    t/m h8

---
+++ src/h10/h10.java
@@ -0,0 +1,17 @@
+package h10;
+
+
+import java.awt.*;
+import java.applet.*;
+
+        public class h10 extends Applet {
+
+            public void init() {
+
+            }
+            public void paint(Graphics g) {
+
+            }
+        }
+
+
