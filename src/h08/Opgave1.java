294ee9aeb44fd775f819476abfcb0c2e9827a28d
ochab <ochabab@roc-dev.com>
2019-10-14 10:06:55 +0200

    t/m h08

--- src/h08/Opgave1.java
+++ src/h08/Opgave1.java
@@ -4,20 +4,24 @@ import java.awt.*;
 import java.applet.*;
 import java.awt.Button.*;
 
+ public class Opgave1 extends Applet {
+     Label label;
+     TextField tekst;
+     Button button1;
+     Button button2;
 
-public class Opgave1 extends Applet {
-    Button knop;
-    Button knop2;
+     public void init() {
+         button1 =  new Button();
+         button1.setLabel("Ok");
+         Button1Listener bl = new Button1Listener();
+         button1.addActionListener( bl );
+         add(button1);
+         button2 = new Button();
+         button2.setLabel("Reset");
+         Button2Listener bl2 = new Button2Listener();
+         button2.addActionListener( bl );
+         add(button2);
+     }
+ }
 
-    public void init() {
-        knop = new Button();
-        knop.setLabel( "Klik op mij" );
-        knop2 = new Button();
-        knop2.setLabel("Click");
-        add(knop);
-    }
 
-    public void paint(Graphics g) {
-        g.drawString( "ik", 20, 20 );
-    }
-}
