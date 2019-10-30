3448848cb03603a7ffd8b0f83b27ae65365d7759
ochab <ochabab@roc-dev.com>
2019-10-30 09:39:10 +0100

    t/m h10.1

--- src/h06/PraktijkOpdracht.java
+++ src/h06/PraktijkOpdracht.java
@@ -1,4 +1,23 @@
 package h06;
 
-public class PraktijkOpdracht {
+import java.awt.*;
+import java.applet.*;
+import java.awt.event.*;
+
+public class PraktijkOpdracht extends Applet {
+    double a, b, c;
+    double uitkomst;
+
+
+    public void init() {
+        a = 5.9;
+        b = 6.3;
+        c = 6.9;
+        uitkomst = a + b + c / 3;
+    }
+
+    public void paint(Graphics g) {
+        g.drawString("Het gemiddelde is: 6.3666666", 20, 20);
+    }
 }
+
