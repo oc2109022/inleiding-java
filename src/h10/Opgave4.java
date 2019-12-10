97a2b417cab0ca8eb82c4cc5aa08a4770a67da78
ochab <ochabab@roc-dev.com>
2019-12-10 11:22:20 +0100

    t/m h12

--- src/h10/Opgave4.java
+++ src/h10/Opgave4.java
@@ -13,6 +13,8 @@ public class Opgave4 extends Applet {
     public void init() {
         tekst1 = new TextField("", 20);
         label1 = new Label("Typ hier uw Getal");
+        label2 = new Label("2de Getal hier typen");
+        label3 = new Label("3de Getal hier typen");
         tekst1.addActionListener(new Tekst1Listener());
         tekst = "";
         add(label1);
