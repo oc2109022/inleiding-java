82d9d699be1efc00a6c47e6eef75aeb2dc3e3402
ochab <ochabab@roc-dev.com>
2019-12-10 11:21:06 +0100

    t/m h12

---
+++ src/h11/Opdracht1.java
@@ -0,0 +1,22 @@
+package h11;
+
+import java.awt.*;
+import java.applet.*;
+
+public class Opdracht1 extends Applet {
+
+    public void init() {}
+
+    public void paint(Graphics g) {
+        g.drawLine(10, 10, 10,100);
+        g.drawLine(20, 20, 20,100);
+        g.drawLine(30, 30, 30,100);
+        g.drawLine(40, 40, 40,100);
+        g.drawLine(50, 50, 50,100);
+        g.drawLine(60, 60, 60,100);
+        g.drawLine(70, 70, 70,100);
+        g.drawLine(80, 80, 80,100);
+        g.drawLine(90, 90, 90,100);
+        g.drawLine(100, 100, 100,100);
+    }
+}
